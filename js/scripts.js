e363b1463a08154402751a06e72463d7dde7a677
zjohnson1 <zjohnson@eagles.ewu.edu>
2014-12-08 15:31:29 -0800

    version 5.1
    added some more styling (font)

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,11 @@ var spokane = '99201';
 var marker = 0;
 var ticker = 1;
 
+function test(form){
+  var zip = form.inputbox.value;
+  alert ("You typed: " + zip);
+}
+
 $('div.ui.menu').hide();
 
 $('div.ui.icon.button').click(function(){
@@ -24,13 +29,13 @@ $.simpleWeather({
       $('.myIcon').hide();
 
       //put temp into h1
-      $('h1').text(weather.temp);
+      $('h1').text(weather.temp + weather.units.temp);
 
       //put high of the day into h2
-      $('.high').text(weather.high);
+      $('.high').text('High ' + weather.high + weather.units.temp);
 
       //put low of the day into h2
-      $('.low').text(weather.low);
+      $('.low').text('Low ' + weather.low + weather.units.temp);
 
 
       //enter 5 day data
@@ -41,11 +46,16 @@ $.simpleWeather({
         $('div a:nth-child(9)').text(weather.forecast[4].day);
 
       //enter 5 day weather description
-        $('div p:nth-child(2)').text(weather.forecast[0].high);
+        $('div p:nth-child(2)').text('High ' + weather.forecast[0].high + ', Low ' + weather.forecast[1].low);
         $('div p:nth-child(4)').text(weather.forecast[1].high);
         $('div p:nth-child(6)').text(weather.forecast[2].high);
         $('div p:nth-child(8)').text(weather.forecast[3].high);
         $('div p:nth-child(10)').text(weather.forecast[4].high);
+
+        if( weather.currently == 'Sunny'){
+          $('img.myIcon.Sunny').show();
+        }
+        else if( weather.currently == 'Cloudy')
 
       
 
