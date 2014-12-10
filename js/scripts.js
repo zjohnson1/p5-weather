77c2c4fceab7fc5bd177391a5262d55f4823f925
zjohnson1 <zjohnson@eagles.ewu.edu>
2014-12-10 10:30:44 -0800

    version 7
    added zip and geolocation features

--- js/scripts.js
+++ js/scripts.js
@@ -1,93 +1,186 @@
-var cheney = '99004';
-var spokane = '99201';
-var marker = 0;
-var ticker = 1;
 
-function test(form){
-  var zip = form.inputbox.value;
-  alert ("You typed: " + zip);
-}
+
 
 $('div.ui.menu').hide();
+$('.myIcon').hide();
 
 $('div.ui.icon.button').click(function(){
   $('div.ui.menu').transition('horizontal flip');
 });
 
-function testResults (div) {
+if (navigator.geolocation) {
+    // Yes! Show button
+    $('.getGeolocation').show(); 
+  } else {
+    // No. Hide button
+    $('.getGeolocation').hide();
+  }
 
-      var zip = div.input.value;
-    
-    }
+$('.getGeolocation').on('click', function() {
+  
+    navigator.geolocation.getCurrentPosition(function(position) {
+    //load weather using your lat/lng coordinates. See _loadWeather()_ below
+    loadWeather(position.coords.latitude+','+position.coords.longitude); 
+    // See latitute & longitude. Note, wait a few seconds
+    console.log(position.coords.latitude+','+position.coords.longitude);
+  });
+  
+});
 
+ var loadWeather = function(location) { 
+  $.simpleWeather({
 
-$.simpleWeather({
+      //create function called testResults which take in a div takes the form in the div
+      //and assigns the value wich should be a number zip code to the variable zip
+      /*function testResults (div) {
 
-    //create function called testResults which take in a div takes the form in the div
-    //and assigns the value wich should be a number zip code to the variable zip
-    /*function testResults (div) {
-
-      var zip = div.input.value;
-    
-    }*/
-
-    
-    location: spokane,
-    woeid: '',
-    unit: 'f',
-    
-    success: function(weather) {
-      //hide all of my icons
-      $('.myIcon').hide();
-
-      //put temp into h1
-      $('h1').text(weather.temp + weather.units.temp);
-
-      //put high of the day into h2
-      $('.high').text('High ' + weather.high + weather.units.temp);
-
-      //put low of the day into h2
-      $('.low').text('Low ' + weather.low + weather.units.temp);
-
-
-      //enter 5 day data
-        $('div a:nth-child(1)').text(weather.forecast[0].day);
-        $('div a:nth-child(3)').text(weather.forecast[1].day);
-        $('div a:nth-child(5)').text(weather.forecast[2].day);
-        $('div a:nth-child(7)').text(weather.forecast[3].day);
-        $('div a:nth-child(9)').text(weather.forecast[4].day);
-
-      //enter 5 day weather description
-        $('div p:nth-child(2)').text('High ' + weather.forecast[0].high + ', Low ' + weather.forecast[1].low);
-        $('div p:nth-child(4)').text(weather.forecast[1].high);
-        $('div p:nth-child(6)').text(weather.forecast[2].high);
-        $('div p:nth-child(8)').text(weather.forecast[3].high);
-        $('div p:nth-child(10)').text(weather.forecast[4].high);
-
-        if( weather.currently == 'Sunny'){
-          $('img.myIcon.Sunny').show();
-        }
-        else if( weather.currently == 'Cloudy'){
-          $('img.myIcon.Cloudy').show();
-        }
-        else if( weather.currently == 'Partly Cloudy'){
-          $('img.myIcon.Partly').show();
-        }
-        else if( weather.currently == 'Rainy'){
-          $('img.myIcon.Rain').show();
-        }
-        else{
-          $('h1').text('Need to add this icon ' + weather.currently);
-        }
-
+        var zip = div.input.value;
+      
+      }*/
       
 
-      console.log(weather);
+      
+      location: location,
+      
+      
+      success: function(weather) {
+        //hide all of my icons
+        
 
-    },
+        //put temp into h1
+        $('h1').text(weather.temp + weather.units.temp);
+
+        //put high of the day into h2
+        $('.high').text('High ' + weather.high + weather.units.temp);
+
+        //put low of the day into h2
+        $('.low').text('Low ' + weather.low + weather.units.temp);
+
+
+        //enter 5 day data
+          $('div a:nth-child(1)').text(weather.forecast[0].day);
+          $('div a:nth-child(3)').text(weather.forecast[1].day);
+          $('div a:nth-child(5)').text(weather.forecast[2].day);
+          $('div a:nth-child(7)').text(weather.forecast[3].day);
+          $('div a:nth-child(9)').text(weather.forecast[4].day);
+
+        //enter 5 day weather description
+          $('div p:nth-child(2)').text('High ' + weather.forecast[0].high + ', Low ' + weather.forecast[1].low);
+          $('div p:nth-child(4)').text(weather.forecast[1].high);
+          $('div p:nth-child(6)').text(weather.forecast[2].high);
+          $('div p:nth-child(8)').text(weather.forecast[3].high);
+          $('div p:nth-child(10)').text(weather.forecast[4].high);
+
+          if( weather.currently == 'Sunny'){
+            $('img.myIcon.Sunny').show();
+          }
+          else if( weather.currently == 'Cloudy'){
+            $('img.myIcon.Cloudy').show();
+          }
+          else if( weather.currently == 'Partly Cloudy'){
+            $('img.myIcon.Partly').show();
+          }
+          else if( weather.currently == 'Rainy'){
+            $('img.myIcon.Rain').show();
+          }
+          else if( weather.currently == 'Mostly Cloudy'){
+            $('img.myIcon.Cloudy').show();
+          }
+          else{
+            $('h1').text('Need to add this icon ' + weather.currently);
+          }
+
+        
+
+        console.log(weather);
+
+      },
+      
+      error: function(error) {
+        $("#weather").html('<p>'+error+'</p>');
+      }
+      
+    });
+  };
+
+$('i.search.icon').on('click', function() {
+
+  var zipcode = $('#getWeather').val();
+$.simpleWeather({
+            
+      location: zipcode,
+      
+      
+      success: function(weather) {
+        //hide all of my icons
+        
+
+        //put temp into h1
+        $('h1').text(weather.temp + weather.units.temp);
+
+        //put high of the day into h2
+        $('.high').text('High ' + weather.high + weather.units.temp);
+
+        //put low of the day into h2
+        $('.low').text('Low ' + weather.low + weather.units.temp);
+
+
+        //enter 5 day data
+          $('div a:nth-child(1)').text(weather.forecast[0].day);
+          $('div a:nth-child(3)').text(weather.forecast[1].day);
+          $('div a:nth-child(5)').text(weather.forecast[2].day);
+          $('div a:nth-child(7)').text(weather.forecast[3].day);
+          $('div a:nth-child(9)').text(weather.forecast[4].day);
+
+        //enter 5 day weather description
+          $('div p:nth-child(2)').text('High ' + weather.forecast[0].high + ', Low ' + weather.forecast[1].low);
+          $('div p:nth-child(4)').text(weather.forecast[1].high);
+          $('div p:nth-child(6)').text(weather.forecast[2].high);
+          $('div p:nth-child(8)').text(weather.forecast[3].high);
+          $('div p:nth-child(10)').text(weather.forecast[4].high);
+
+          if( weather.currently == 'Sunny'){
+            $('img.myIcon.Sunny').show();
+          }
+          else if( weather.currently == 'Cloudy'){
+            $('img.myIcon.Cloudy').show();
+          }
+          else if( weather.currently == 'Partly Cloudy'){
+            $('img.myIcon.Partly').show();
+          }
+          else if( weather.currently == 'Rainy'){
+            $('img.myIcon.Rain').show();
+          }
+          else if( weather.currently == 'Mostly Cloudy'){
+            $('img.myIcon.Cloudy').show();
+          }
+          else{
+            $('h1').text(weather.currently);
+          }
+
+        
+
+        console.log(weather);
+
+      },
+      
+      error: function(error) {
+        $("#weather").html('<p>'+error+'</p>');
+      }
+      
+    });
+
+
+
+$('#getWeather').val('');
+  
+});
+
+
+
+
+
+
+
+
     
-    error: function(error) {
-      $("#weather").html('<p>'+error+'</p>');
-    }
-    
-  });
